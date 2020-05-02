a6c52f13d65b8f1e71343f2e4a8f0f4d4974e9b5
labrosb <labros_b@hotmail.com>
2020-05-02 16:44:23 +0200

    Updates

---
+++ precache-manifest.ff68317d266c6f42458023bcb95a4527.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a757da9fa10a22dcf3ce0cdacd8f1b59",
+    "url": "/Music-albums/index.html"
+  },
+  {
+    "revision": "95a351eb049717462850",
+    "url": "/Music-albums/static/css/main.52fed2b2.chunk.css"
+  },
+  {
+    "revision": "727133c6daacc519e481",
+    "url": "/Music-albums/static/js/2.fbd9776b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Music-albums/static/js/2.fbd9776b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a351eb049717462850",
+    "url": "/Music-albums/static/js/main.477d60f3.chunk.js"
+  },
+  {
+    "revision": "13397d4e098ef29791b0",
+    "url": "/Music-albums/static/js/runtime-main.6eb1072d.js"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/Music-albums/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/Music-albums/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "4e722e123489f1e7b25250e30a71d135",
+    "url": "/Music-albums/static/media/loading.4e722e12.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c245b7d67c70880b97a5bbf53d9574a8.js"
+  "/Music-albums/precache-manifest.ff68317d266c6f42458023bcb95a4527.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Music-albums/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
